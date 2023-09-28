8435fe9e888db0541529dfe0992f1b50b0480953
Kimrong <rlagusdn3220@naver.com>
2023-09-28 16:23:28 +0900

    저장빼고 다 함

--- KHW.py
+++ KHW.py
@@ -1,6 +1,7 @@
 import sys
 import cv2 as cv
 import time
+import numpy as np
 
 #이름과 경로
 path="./data/"
@@ -17,45 +18,81 @@ ret2, frame2 = cap2.read()
 #프레임 관련 변수
 tot_frame = int(cap1.get(cv.CAP_PROP_FRAME_COUNT))
 fps=int(cap1.get(cv.CAP_PROP_FPS))
-bias=10
+bias=15
 delay=int(1000/fps)-bias
+count=0
+frame_pos=0
 
-#화면에 프레임을 표시하기 위한 변수
+#저장을 위한 변수
+frame_width = int(cap1.get(3))
+frame_height = int(cap1.get(4))
+fourcc = cap1.get(cv.CAP_PROP_FOURCC)
+out = cv.VideoWriter('team_num_2.mp4', int(fourcc), fps, (frame_width, frame_height))
+
+#화면에 텍스트를 표시하기 위한 변수
 text_pos = (20, 40)  # 텍스트가 추가될 좌표 (x, y)
 font = cv.FONT_HERSHEY_SIMPLEX  # 폰트 선택
 font_scale = 1  # 폰트 크기
 font_color = (0, 0, 254)  # 폰트 색상 (BGR 형식)
 font_thickness = 2  # 폰트 두께
 
+#밝기 관련 변수
+brightness=10.0
+
+def callback_brightness(x):
+    global brightness
+    brightness=2**(x/10.0)-1
+def callback_playbar(x):
+    global frame_pos
+    frame_pos=x
+
 if cap1.isOpened() and cap2.isOpened():
-    s_time=time.time()
+    s_time=time.time()  #시작시간 기록
+    #윈도우와 트랙바 생성
+    win=cv.namedWindow(winname)
+    cv.createTrackbar("brightness", winname, 10, 20, callback_brightness)
+    cv.createTrackbar("playbar", winname, 0, tot_frame, callback_playbar)
+
     while ret1 or ret2:
+        # 프레임 위치에 따라 재생바의 위치도 변경
+        cv.setTrackbarPos("playbar",winname,frame_pos)
+        # 2번 프레임에 밝기 변화 추가
+        scaled_frame=cv.convertScaleAbs(frame2,alpha=brightness,beta=0)
 
         # 이미지에 텍스트 추가
-        text_frame_1 = cv.putText(frame1, str(int(cap1.get(cv.CAP_PROP_POS_FRAMES))), text_pos, font, font_scale, font_color,
+        count+=1
+        text_frame1 = cv.putText(frame1, "org_index="+str(int(cap1.get(cv.CAP_PROP_POS_FRAMES))), text_pos, font, font_scale, font_color,
                                   font_thickness)
-        text_frame_2 = cv.putText(frame2, str(int(cap2.get(cv.CAP_PROP_POS_FRAMES))), text_pos, font, font_scale,
+        text_frame2 = cv.putText(scaled_frame, "this_index="+str(count), text_pos, font, font_scale,
                                   font_color, font_thickness)
+
         #프레임 합쳐서 윈도우에 띄우기
-        merged_image = cv.hconcat([text_frame_1, text_frame_2])
-        cv.imshow(winname,merged_image)
+        merged_frame = cv.hconcat([text_frame1, text_frame2])
+        cv.imshow(winname,merged_frame)
+        out.write(merged_frame) #프레임 저장
 
         #"esc"로 종료, "space"로 일시정지
         key = cv.waitKey(delay)
         if key==27:
-            break
+            sys.exit()
         elif key==32:
             while True:
                 key = cv.waitKey()
                 if key==32:
                     break
+                elif key == 27:
+                    sys.exit()
+                time.sleep(0.1)
 
         #프레임 받아오기
         ret1, frame1 = cap1.read()
         ret2, frame2 = cap2.read()
+        frame_pos+=1
+        cap1.set(cv.CAP_PROP_POS_FRAMES, frame_pos)
+        cap2.set(cv.CAP_PROP_POS_FRAMES, frame_pos)
 
     #원래 동영상의 시간과 실제 재생시간의 차이 출력
-    print("original time = 44sec")
+    print("total playtime = 44sec")
     runtime=time.time()-s_time
     print(f"run time = {runtime}sec")
 
